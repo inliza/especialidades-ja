6c8e0ad28356636af4bac10a86aa373b3790d008
Edwin Lizardo <iamlizardo@Edwins-MacBook.local>
2025-03-11 20:58:38 -0400

    url del servicio de arriba

--- src/app/global.ts
+++ src/app/global.ts
@@ -1,10 +1,10 @@
 export class Globals {
     a = {
         Pruebas: "",
-        PruebasRemote: "https://api-cf-dev.up.railway.app/api/",
+        PruebasRemote: "https://especialidades-ja-api-production.up.railway.app/api/",
         Produccion: "Inserta la url de prod aqui",
         API: `http://localhost:3555/api/`,
     };
-    public urlApi = this.a["API"];
+    public urlApi = this.a["PruebasRemote"];
 
 }
